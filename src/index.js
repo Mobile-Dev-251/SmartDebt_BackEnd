e39e8c2bd3dc0ad3ac531ca7cfda3b76573a15ae
Hoang Phuong Binh <164758713+binh2308@users.noreply.github.com>
2026-01-06 16:26:45 +0700

    Restore imports and Swagger setup in index.js
    Restored import statements and Swagger configuration for the API documentation.

--- src/index.js
+++ src/index.js
@@ -27,10 +27,10 @@ const swaggerOptions = {
       description: "API Documentation for SmartDebt Application",
     },
     servers: [
-      // {
-      //   url: "https://app-nodejs-smartdebt-backend-f5a5f6d5fteddea8.eastasia-01.azurewebsites.net", // URL thực tế trên Azure
-      //   description: "Production server",
-      // },
+      {
+        url: "https://app-nodejs-smartdebt-backend-f5a5f6d5fteddea8.eastasia-01.azurewebsites.net", // URL thực tế trên Azure
+        description: "Production server",
+      },
       {
         url: `http://localhost:${port}`,
         description: "Local development server",
@@ -74,3 +74,4 @@ app.use("/categories", categoryRoutes);
 app.listen(port, () => {
   console.log("Server is running on http://localhost:" + port);
 });
+
